02f7cfacd3bcba050d36215b1ac118c23c57d415
JArrod <jarrod.haynes1@gmail.com>
2020-08-04 09:15:16 +1000

    Update closestTherapist.py
    Updated to iterate through 10 days worth of data exports and find the closest date

--- closestTherapist.py
+++ closestTherapist.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import googlemaps
+from datetime import date, timedelta
 
 api_key = ''
 gmaps = googlemaps.Client(key=api_key)
+today = date.today()
 
-df = pd.read_csv(r'C:\Users\info\Downloads\therapists.csv', skiprows=1, skipfooter=3)
+#Find most recent file
+extension = r'C:\Users\info\OneDrive\1. M2M Administration\EXPORTED FROM SOFTWARE\Therapist Data\Maps_Export_'
+
+#Extract most recent therapist data up to 10 days ago
+i = 0
+while i < 10:
+    dt = date.today() - timedelta(i)
+    df = pd.read_csv(fr'{extension}{dt.strftime("%d-%m-%Y")}.csv', skiprows=1, skipfooter=3, engine='python')
+    if len(df) > 1:
+        break
+    i += 1
+    if i == 10:
+        print("Checked 10 days worth of data -- please extract more recent data")
 
 #Drop everyone expired or unavail
-df = df.dropna(subset=['Insurance Expiry Date', 'First Aid Expiry Date', 'Police Check Expiry', 'Membership Expiry'])
+df = df.dropna(subset=['Insurance Expiry Date', 'First Aid Expiry', 'Police Check Expiry', 'Membership Expiry'])
 df = df[~df['Client Type'].str.match('Expired')]
 df = df[~df['Client Type'].str.match('Unavailable')]
 #Change Mobile column to international number
 df['Mobile'] = df['Mobile'].str.replace('04', '+614', 1)
 df['Mobile'] = df['Mobile'].str.replace(' ', '')
-
-print('Please input a State')
+print('input state')
 state = input()
+
 print('Please input an address')
 dest = input()
 #drop rows that don't match the state of new client
@@ -36,7 +50,8 @@ for i in match['Address']:
 match['distance'] = dist
 match['duration'] = dur
 #Convert format to mins
-match['Minutes'] = pd.eval(match['duration'].replace(['hours?', 'mins', 'min'], ['*60+', '', ''], regex=True))
+match['Minutes'] = pd.eval(match['duration'].replace(['hours?', 'mins', 'min'], ['*60+', '', ''], regex=True)).astype(int)
+match = match.sort_values(by=['Minutes']).reset_index(drop=True)
 print(match[['First Name-', 'duration', 'distance', 'Minutes']])
 best_fit = match.nsmallest(4, 'Minutes')
 print('--------------------------')
